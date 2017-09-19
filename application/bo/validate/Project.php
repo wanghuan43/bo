279f4ff2390ebccd8467704503e7df7592c91d20
zhichang.gao <reman0214@163.com>
2017-09-19 14:10:13 +0800

    合同新建、列表页

---
+++ application/bo/validate/Project.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhichang
+ * Date: 2017/9/19
+ * Time: 上午10:31
+ */
+
+namespace app\bo\validate;
+
+
+use think\Validate;
+
+class Project extends Validate
+{
+
+    protected $rule = [
+        'p_no' => 'require',
+        'p_name' => 'require'
+    ];
+
+    protected $message = [
+        'p_no' => '编号不能为空',
+        'p_name' => '名称不能为空'
+    ];
+
+}
