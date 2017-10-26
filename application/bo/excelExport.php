ec4c3078feb02264edd8c1e9cc57d7cf1db18ae4
zhichang <zhichang.gao@hotmail.com>
2017-10-26 13:27:04 +0800

    我的订单、合同Excel导出

---
+++ application/bo/excelExport.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhichang
+ * Date: 2017/10/24
+ * Time: 下午2:09
+ */
+
+return [
+    'boExcel' => [
+        'orders' => [
+            'A' => ['title'=>'部门','key'=>'o_dname'],
+            'B' => ['title'=>'责任人','key'=>'o_mname'],
+            'C' => ['title'=>'项目号','key'=>'p_no'],
+            'D' => ['title'=>'项目名称','key'=>'o_pname'],
+            'E' => ['title'=>'对方名称','key'=>'o_coname'],
+            'F' => ['title'=>'关键字','key'=>'tags'],
+            'G' => ['title'=>'内部','key'=>'o_lie'],
+            'H' => ['title'=>'赢单机会','key'=>'cs_name'],
+            'I' => ['title'=>'商机状态','key'=>'o_status'],
+            'J' => ['title'=>'行动日','key'=>'o_date'],
+            'K' => ['title'=>'购销','key'=>'o_type'],
+            'L' => ['title'=>'发生金额','key'=>'op_used'],
+            'M' => ['title'=>'发生日(计划)','key'=>'op_date'],
+            'N' => ['title'=>'商务阶段','key'=>'op_type']
+        ],
+        'contract' => [
+            'A' => ['title'=>'部门','key'=>'o_dname'],
+            'B' => ['title'=>'责任人','key'=>'o_mname'],
+            'C' => ['title'=>'项目号','key'=>'p_no'],
+            'D' => ['title'=>'项目名称','key'=>'o_pname'],
+            'E' => ['title'=>'对方名称','key'=>'o_coname'],
+            'F' => ['title'=>'关键字','key'=>'tags'],
+            'G' => ['title'=>'内部','key'=>'o_lie'],
+            'H' => ['title'=>'赢单机会','key'=>'cs_name'],
+            'I' => ['title'=>'商机状态','key'=>'o_status'],
+            'J' => ['title'=>'行动日','key'=>'o_date'],
+            'K' => ['title'=>'购销','key'=>'o_type'],
+            'L' => ['title'=>'发生金额','key'=>'ou_used'],
+            'M' => ['title'=>'发生日','key'=>'ou_date'],
+            'N' => ['title'=>'备注','key'=>'c_bakup'],
+            'O' => ['title'=>'商务阶段','key'=>'ou_type'],
+            //'P' => ['title'=>'原定日','key'=>''],
+            //'Q' => ['title'=>'记账月','key'=>''],
+            'P' => ['title'=>'合同号','key'=>'c_no'],
+            'Q' => ['title'=>'合同名称','key'=>'c_name']
+        ]
+    ],
+];
