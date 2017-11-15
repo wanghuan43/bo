b79dbcc60a0cda6fc9d1ec9468dd04d6d0ead701
zhichang <zhichang.gao@hotmail.com>
2017-11-15 15:09:54 +0800

    列表页选择功能

---
+++ public/static/js/all.js
@@ -0,0 +1,69 @@
+(function($){
+
+    var selIDs = {
+
+        e : "#main-container .selected-ids select",
+        add : function(name,value){
+            this.remove(value);
+            $(this.e).append('<option value="'+value+'" selected>'+name+'</option>');
+        },
+        remove : function(value){
+            $(this.e).find("option[value='"+value+"']").remove();
+        },
+        val : function(){
+            return $(this.e).val();
+        }
+
+    };
+
+    var allChecked = true;
+
+    $("#main-container .lists-id").each(function(){
+        if(in_array($(this).val(),selIDs.val())){
+            $(this).prop("checked",true);
+        }else{
+            allChecked = false
+        }
+    });
+
+    if(allChecked==true){
+        $("#main-container .checkAll").prop("checked",true);
+    }
+
+    var checkAllChecked = function(){
+        var ret = true;
+        $("#main-container .lists-id").each(function(){
+            if($(this).prop("checked")==false){
+                ret = false;
+            }
+        });
+        return ret;
+    };
+
+    $("#main-container .lists-id").click(function(){
+        var flag = $(this).prop("checked");
+        if( flag == true ){
+            selIDs.add($(this).attr("data-name"),$(this).val());
+            if(checkAllChecked()){
+                $("#main-container .checkAll").prop("checked",true);
+            }
+        }else{
+            selIDs.remove($(this).val());
+            $("#main-container .checkAll").prop("checked",false);
+        }
+    });
+
+    $("#main-container .checkAll").click(function(){
+        var flag = $(this).prop("checked");
+        $("#main-container .lists-id").prop("checked",flag);
+        if(flag == true){
+            $("#main-container .lists-id").each(function(){
+                selIDs.add($(this).attr("data-name"),$(this).val());
+            });
+        }else{
+            $("#main-container .lists-id").each(function(){
+                selIDs.remove($(this).val());
+            });
+        }
+    });
+})(jQuery);
