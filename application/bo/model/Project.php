cbbcf181b9d51ee4e90f1bdda488fcfcba2d6deb
jerry <175118576@qq.com>
2017-12-13 10:32:58 +0800

    财务预算

--- application/bo/model/Project.php
+++ application/bo/model/Project.php
@@ -72,7 +72,7 @@ class Project extends BoModel
         $member = $this->getCurrent();
         $this->alias('p');
         if ($member->m_isAdmin == "2") {
-            $this->join('__CIRCULATION__ c', "p.p_id = c.ci_otid AND c.ci_type = 'project' AND c.ci_mid".$member->m_id, "left");
+            $this->join('__CIRCULATION__ c', "p.p_id = c.ci_otid AND c.ci_type = 'project' AND c.ci_mid=".$member->m_id, "left");
             $this->where("p.p_mid", "=", $member->m_id);
         }
         $this->field("p.*");
