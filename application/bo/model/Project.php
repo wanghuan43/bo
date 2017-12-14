31435e67cf96e5e051ba009933d3ec7fb20a6587
reman0214 <reman0214@163.com>
2017-12-14 10:25:18 +0800

    修复项目getList取数据逻辑错误的BUG

--- application/bo/model/Project.php
+++ application/bo/model/Project.php
@@ -73,7 +73,8 @@ class Project extends BoModel
         $this->alias('p');
         if ($member->m_isAdmin == "2") {
             $this->join('__CIRCULATION__ c', "p.p_id = c.ci_otid", 'left');
-            $this->where("c.ci_mid|p.p_mid", "=", $member->m_id)->where('c.ci_type', "=", "project")
+            $this->where(['c.ci_mid'=>['=',$member->m_id],'c.ci_type'=>['=','project']])
+                ->whereOr('p.p_mid', "=", $member->m_id)
                 ->group('p.p_id');
         }
         $this->field("p.*");
