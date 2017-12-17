a5fd7e00f446794c6e3932cb3e87787e5f08350b
jerry <175118576@qq.com>
2017-12-17 16:59:29 +0800

    财务预算

--- application/bo/model/Acceptance.php
+++ application/bo/model/Acceptance.php
@@ -89,7 +89,7 @@ class Acceptance extends BoModel
         $this->alias('a');
         if ($member->m_isAdmin == "2") {
             $this->join('__CIRCULATION__ c', "a.a_id = c.ci_otid", "left");
-            $this->where("((c.ci_mid = ".$member->m_id." and c.ci_type = 'acceptance') OR a.a_mid = ".$member->m_id.")")
+            $this->where("(a.a_id IN (SELECT ci_otid FROM kj_circulation WHERE ci_mid = ".$member->m_id." and ci_type = 'acceptance') OR a.a_mid = ".$member->m_id.")")
                 ->group('a.a_id');
         }
         $this->field("a.*");
