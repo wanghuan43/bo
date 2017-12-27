e83d52c8288d3cfa7459b9bc20a9b1890d43fb27
jerry <175118576@qq.com>
2017-12-27 16:49:53 +0800

    财务预算

--- application/bo/libs/ReportEntity.php
+++ application/bo/libs/ReportEntity.php
@@ -82,7 +82,7 @@ class ReportEntity extends BoModel
                         } elseif ($opt == "like") {
                             $val = "%$val%";
                         }
-                        if(in_array($value, ['i_type', 'i_tax', 'c_type', 'a_type', 'r_type', 'o_type','m_isAdmin']) AND empty($val)){
+                        if(in_array($value, ['i_type', 'i_tax', 'c_type', 'a_type', 'r_type', 'o_type','m_isAdmin','o_tax']) AND empty($val)){
                             break;
                         }
                         $mtmp->where($f . "." . $value, $opt, $val);
