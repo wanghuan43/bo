5d9c9825e036129b682fe0bbeb622c95a3f99a81
zhichang <zhichang.gao@hotmail.com>
2017-12-28 09:46:36 +0800

    修复导出时筛选的BUG

--- public/static/js/lists.js
+++ public/static/js/lists.js
@@ -1,6 +1,41 @@
 /**
  * 列表页JS.
  */
+(function (original) {
+
+    jQuery.fn.clone = function () {
+
+        var result = original.apply(this, arguments),
+
+            my_textareas = this.find('textarea').add(this.filter('textarea')),
+
+            result_textareas = result.find('textarea').add(result.filter('textarea')),
+
+            my_selects = this.find('select').add(this.filter('select')),
+
+            result_selects = result.find('select').add(result.filter('select'));
+
+        for (var i = 0, l = my_textareas.length; i < l; ++i) $(result_textareas[i]).val($(my_textareas[i]).val());
+
+        for (var i = 0, l = my_selects.length;   i < l; ++i) {
+
+            for (var j = 0, m = my_selects[i].options.length; j < m; ++j) {
+
+                if (my_selects[i].options[j].selected === true) {
+
+                    result_selects[i].options[j].selected = true;
+
+                }
+
+            }
+
+        }
+
+        return result;
+
+    };
+
+}) (jQuery.fn.clone);
 
 //删除功能
 (function ($) {
@@ -66,9 +101,10 @@
                 msg = '是否要导出全部内容?';
             }
         }
+
         custom.confirm(msg,function(){
             $("#for-export").html(form.clone());
-            form = $("#for-export").find("form");
+            form = $("#for-export").find("form");K
             form.attr({ 'id':type+'AllForm2',"action":action,"method":"POST"});
             form.find(".selected-ids select").attr("name","ids[]");
             form.append("<input type='hidden' name='operator' value='export'/>");
