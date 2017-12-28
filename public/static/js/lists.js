7c7dc58e5290ee1b1fa1c18150e23b56806f6453
zhichang <zhichang.gao@hotmail.com>
2017-12-28 09:59:54 +0800

    修复JS多打了个K字符的BUG

--- public/static/js/lists.js
+++ public/static/js/lists.js
@@ -104,7 +104,7 @@
 
         custom.confirm(msg,function(){
             $("#for-export").html(form.clone());
-            form = $("#for-export").find("form");K
+            form = $("#for-export").find("form");
             form.attr({ 'id':type+'AllForm2',"action":action,"method":"POST"});
             form.find(".selected-ids select").attr("name","ids[]");
             form.append("<input type='hidden' name='operator' value='export'/>");
