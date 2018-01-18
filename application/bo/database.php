d837a8c4f727ea06c88afb6396fd82b0d2387b88
jerry <175118576@qq.com>
2018-01-18 14:17:51 +0800

    flow切换专用数据库

--- application/bo/database.php
+++ application/bo/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '47.91.20.83',
     // 数据库名
-    'database'        => 'bo',
+    'database'        => 'flow',
     // 用户名
     'username'        => 'root',
     // 密码
